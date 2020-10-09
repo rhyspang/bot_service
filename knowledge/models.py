9a3a6846b0c5ac9e35467994ee50d70ce777dcfc
Pang Sheng <pangsheng@didichuxing.com>
2020-10-09 21:28:30 +0800

    init

---
+++ knowledge/models.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class KnowledgeBase(models.Model):
+
+    name = models.CharField(max_length=128)
+    shop_id = models.CharField(max_length=256)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    running_status = models.BooleanField(default=True)
+
+
+class KnowledgeItem(models.Model):
+
+    KNOWLEDGE_SCOPE = (
+        (0, '售前'),
+        (1, '售中'),
+        (1, '售后'),
+    )
+
+    knowledge_base = models.ForeignKey(
+        KnowledgeBase, on_delete=models.SET_NULL, null=True)
+    question = models.CharField(max_length=512)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    scope = models.IntegerField(choices=KNOWLEDGE_SCOPE)
+
+
+class Answer(models.Model):
+    knowledge_item = models.ForeignKey(
+        KnowledgeItem, on_delete=models.SET_NULL, null=True)
+    content = models.TextField()
+
+
+class SimilarQuestion(models.Model):
+    knowledge_item = models.ForeignKey(
+        KnowledgeItem, on_delete=models.SET_NULL, null=True)
+    content = models.TextField()
