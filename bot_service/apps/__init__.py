be4056772d2eb8e3be6486cefea7daa87ae7dad4
Pang Sheng <pangsheng@didichuxing.com>
2020-10-16 22:23:49 +0800

    rm copyright

--- bot_service/apps/__init__.py
+++ bot_service/apps/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020 Didichuxing Inc.
-# Author: Sheng Pang (pangsheng@didiglobal.com)
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
